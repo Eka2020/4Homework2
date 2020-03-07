acc44a86330084ed0351d1a41fef9e02f8e150dc
Eka2020 <60359521+Eka2020@users.noreply.github.com>
2020-03-07 23:13:05 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Запуск Эстафеты ");
+        try {
+            for (int i = 1; i <= 5; i++) {
+                Runner runner = new Runner("Бегун " + i);
+                runner.setRunnerNext(i + 1);
+                runner.setRunnerBack(0);
+                runner.start();
+                runner.join();
+            }
+            for (int i = 5; i > 1; i--) {
+                Runner runner = new Runner("Бегун " + i);
+                runner.setRunnerNext(i - 1);
+                runner.setRunnerBack(1);
+                runner.start();
+                runner.join();
+            }
+        } catch (Exception z) {
+            System.out.println("Завершение " + Thread.currentThread().getName());
+        }
+        System.out.println("Бегуну 1 берет палочку");
+        System.out.println("Завершение Эстафеты");
+    }
+}
---
+++ src/com/company/Runner.java
@@ -0,0 +1,51 @@
+package com.company;
+
+public class Runner extends Thread {
+    public Runner(String name) {
+        super(name);
+    }
+
+    private int runnerBack;
+    private int runnerNext;
+
+    public int getRunnerBack() {
+        return runnerBack;
+    }
+
+    public void setRunnerBack(int runnerBack) {
+        this.runnerBack = runnerBack;
+    }
+
+    public int getRunnerNext() {
+        return runnerNext;
+    }
+
+    public void setRunnerNext(int runnerNext) {
+        this.runnerNext = runnerNext;
+    }
+
+    @Override
+    public void run() {
+        try {
+            if (getRunnerBack() == 0) {
+                if (getRunnerNext() == 6) {
+                    System.out.println(this.getName() + " берет палочку ");
+                    System.out.println(this.getName() + " бежит к" + " финишу");
+                } else {
+                    System.out.println(this.getName() + " берет палочку ");
+                    System.out.println(this.getName() + " бежит к" + " Бегуну " + getRunnerNext());
+                }
+            } else {
+                if (getRunnerNext() != 4) {
+                    System.out.println(this.getName() + " берет палочку ");
+                    System.out.println(this.getName() + " бежит к" + " Бегуну " + getRunnerNext());
+                } else {
+                    System.out.println(this.getName() + " бежит к" + " Бегуну " + getRunnerNext());
+                }
+            }
+            sleep(10);
+        } catch (InterruptedException beg) {
+
+        }
+    }
+}
